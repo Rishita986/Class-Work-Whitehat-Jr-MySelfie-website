5cf2388dc7f0e741b16bd9be2623264d7ae719f2
Rishita986 <94119947+Rishita986@users.noreply.github.com>
2021-12-23 19:46:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,15 +12,24 @@ recognition.onresult = function run(event) {
     var Content = event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
-    speak();
+    if(Content=="take my selfie")
+    {
+        console.log("taking selfie ---");
+        speak();
+    }
+    
 }
 
 function speak(){
 var synth = window.speechSynthesis;
-speak_data = document.getElementById("textbox").value;
+speak_data = "Taking selfie, hold on for 3 seconds!";
 var utterThis = new speechSynthesisUtterance(speak_data);
 synth.speak(utterThis);
 Webcam.attach(camera);
+setTimeout(function(){
+    take_snapshot();
+    save();
+},3000);
 }
 
 Webcam.set({
@@ -30,3 +39,19 @@ Webcam.set({
     jpeg_quality: 100
 });
 camera = document.getElementById("camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img id='selfie_image' src='"+data_uri+"'>";
+    });
+    
+}
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src ;
+    link.href = image;
+    link.click();
+}
